4e5883bd9fe2732fa1fae6c48a66e915f4fd7308
estee100 <sktang@ualberta.ca>
2024-08-06 17:43:41 -0600

    allowed for cors headers

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,14 @@ app.use(
     })
 );
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://butterfingers-app.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 app.get("/", (req, res) => {
     res.json({ data: "hello" });
 });
